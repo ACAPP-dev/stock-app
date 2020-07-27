d2c8924a73968d101881b9ff2b98f23527360f3d
ACAPP-dev <acapp909@gmail.com>
2020-07-27 16:32:11 -0700

    added amcharts module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// from to in stock data is unix time from 6/1/20 to 6/15/20
+const FINNHUB_STOCK_DATA_URL = 'https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=D&from=1590969600&to=1592179200&token=bsfleivrh5rf14r5rh80'
+const FINNHUB_API_KEY = 'bsfleivrh5rf14r5rh80'
+
+class App extends React.Component {
+  
+  componentDidMount() {
+    fetch(FINNHUB_STOCK_DATA_URL)
+    .then(resp => resp.json())
+    .then(json => {
+      console.log(json)
+    })
+    
+    //sample api request from finnhub.io documentation
+    // const request = require('request');
+
+    // request('https://finnhub.io/api/v1/stock/candle?symbol=AAPL&resolution=1&from=1572651390&to=1572910590&token=bsfleivrh5rf14r5rh80', { json: true }, (err, res, body) => {
+    //   if (err) { return console.log(err); }
+    //   console.log(body.url);
+    //   console.log(body.explanation);
+    // });
+
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Andrew's Final Project</h1>
+          <h2>Stock App</h2>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
