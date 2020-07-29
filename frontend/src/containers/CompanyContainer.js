e5a4cb9891834b0f5dec958cf8e968f978867937
ACAPP-dev <acapp909@gmail.com>
2020-07-29 14:19:52 -0700

    separate chart component code

---
+++ frontend/src/containers/CompanyContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Chart from '../components/company/Chart'
+
+class CompanyContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h2>Company Container</h2>
+                < Chart />
+            </div>
+        )
+    }
+    
+}
+
+export default CompanyContainer
